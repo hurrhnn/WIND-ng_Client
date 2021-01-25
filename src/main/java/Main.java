55524dfdf91b78f2073bc95335d3579d5b05c0ee
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2021-01-26 02:57:15 +0900

    Main - Prepare to Connecting Websocket Client

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import org.java_websocket.client.WebSocketClient;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+
+        File tokenFile = new File(".WINDToken");
+        boolean isTokenSaved = tokenFile.exists();
+
+        Scanner scanner = new Scanner(System.in);
+        String token = null;
+
+        if (!isTokenSaved) {
+            System.out.print("Input Auth Token: ");
+            token = scanner.nextLine();
+
+            System.out.print("Token is not saved. Save Token? [Y/N]: ");
+            if (scanner.next().toLowerCase().charAt(0) == 'y') {
+                if (tokenFile.createNewFile()) {
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tokenFile));
+                    bufferedWriter.write(token);
+                    bufferedWriter.close();
+                    System.out.println("Saved Token at .WINDToken File.\n");
+                } else
+                    System.out.println("E: Token was not saved successfully.");
+            }
+        } else {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(tokenFile))) {
+                token = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        WebSocketClient webSocketClient = new WINDWebSocketClient(new URI("ws://192.168.0.95:9000"), scanner, token);
+        webSocketClient.connect();
+    }
+}
