988cef368b6dd006f060cbe7011ad4413195455b
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2021-03-04 22:37:22 +0900

    WINDWebSocketClient - Added a lot of features. BlockingQueue used to communicate with ReceiveProtocolMessageThread, and WIND Client-required objects, and exception handling via Logger is added.

--- src/main/java/WINDWebSocketClient.java
+++ src/main/java/WINDWebSocketClient.java
@@ -1,15 +1,28 @@
+import javafx.application.Platform;
+import objects.data.DMessage;
+import objects.data.SelfUser;
+import objects.data.User;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONObject;
+import org.slf4j.Logger;
 
 import java.net.URI;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class WINDWebSocketClient extends org.java_websocket.client.WebSocketClient {
 
+    private final Logger LOGGER = utils.Logger.getLogger(this.getClass());
+
     final public URI serverUri;
-    Thread sendProtocolMessageThread = null;
+    private final String token;
     Scanner scanner;
-    String token;
+
+    private ArrayBlockingQueue<JSONObject> messageQueue;
+    Thread sendProtocolMessageThread = null;
+
+    volatile public int currentVBox = 0;
+    volatile public boolean isReceiveFromMe = false;
 
     public WINDWebSocketClient(URI serverUri, Scanner scanner, String token) {
         super(serverUri);
@@ -18,29 +31,63 @@ public class WINDWebSocketClient extends org.java_websocket.client.WebSocketClie
         this.token = token;
     }
 
+    public SelfUser selfUser;
+    public List<User> userList = new ArrayList<>();
+
+    public Map<Integer, List<DMessage>> DMDatabase = new HashMap<>();
+    WINDClientHelper windObjectGetter = new WINDClientHelper(this);
+
     @Override
     public void onOpen(ServerHandshake handshake) {
-        System.out.println("WebSocket Opened: " + handshake.getHttpStatus() + ", " + handshake.getHttpStatusMessage());
+        LOGGER.info("WebSocket Opened: " + handshake.getHttpStatus() + ", " + handshake.getHttpStatusMessage());
         onAuth(this);
 
+        ClientUI clientUI = new ClientUI();
+        Thread uiThread = new Thread(clientUI);
+        uiThread.setDaemon(true);
+        uiThread.start();
+
+        Platform.startup(() -> {});
+        messageQueue = new ArrayBlockingQueue<>(100);
+        ReceiveProtocolMessageThread receiveProtocolMessageThread = new ReceiveProtocolMessageThread(this, messageQueue);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                if (selfUser == null) {
+                    LOGGER.error("Timeout: Unable to authenticate with server, Shutdown the Program.");
+                    System.exit(1);
+                }
+            } catch (InterruptedException e) {
+                onError(e);
+            }
+        }).start();
+        new Thread(receiveProtocolMessageThread).start();
         sendProtocolMessageThread = new SendProtocolMessageThread(this);
         sendProtocolMessageThread.start();
     }
 
     @Override
     public void onMessage(String message) {
-        JSONObject jsonObject = new JSONObject(message);
-        System.out.println(jsonObject.toString(4));
+        try {
+            JSONObject jsonObject = new JSONObject(message);
+            messageQueue.put(jsonObject);
+        } catch (InterruptedException e) {
+            onError(e);
+        }
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        System.out.println("WebSocket Closed: " + code + ", " + reason);
+        LOGGER.error("WebSocket Closed: " + code + ", " + (reason.isEmpty() ? "Shutdown the Program." : (reason + ", Shutdown the Program.")));
+        System.exit(0);
     }
 
     @Override
     public void onError(Exception e) {
-        e.printStackTrace();
+        LOGGER.error("An error has occurred!");
+        LOGGER.error(e.getClass().getCanonicalName() + ": " + e.getMessage());
+        System.exit(1);
     }
 
     private void onAuth(WINDWebSocketClient windWebSocketClient) {
